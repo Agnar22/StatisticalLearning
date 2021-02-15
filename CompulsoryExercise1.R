4d1cbac9dc65b73b211340362045f3587dcd0656
Agnar22 <agnar.bjornstad@gmail.com>
2021-02-15 13:21:18 +0100

    Install packages.

---
+++ CompulsoryExercise1.R
@@ -0,0 +1,8 @@
+install.packages("knitr")  #probably already installed
+install.packages("rmarkdown")  #probably already installed
+install.packages("ggplot2")  #plotting with ggplot
+install.packages("ggfortify")
+install.packages("MASS")
+install.packages("class")
+install.packages("pROC")
+install.packages("plotROC")
