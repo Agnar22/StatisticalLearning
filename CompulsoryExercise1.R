0cac6de0f8cd236e72cbc63d4cdaa256b451060d
Sebastian Aas <sebastian.aas@outlook.com>
2021-02-15 18:55:02 +0100

    Finished with parts of problem 2,3 and 5

--- CompulsoryExercise1.R
+++ CompulsoryExercise1.R
@@ -7,12 +7,21 @@
 #install.packages("pROC")
 #install.packages("plotROC")
 #install.packages("caret")
+#install.packages("dplyr")
 
 library(ggplot2)
 library(caret)
 library(MASS)
 library(class)
 library(pROC)
+library(ElemStatLearn)
+library(knitr)
+library(kableExtra)
+library(dplyr)
+library(ggplot2)
+library(GGally)
+library(MASS)
+library(boot)
 
 
 # Problem 1.
@@ -94,7 +103,7 @@ summary(logReg)
 # ii)
 
 create_confusion_matrix <- function(pred, target) {
-  confMat <- table(pred, test$diabetes)
+  confMat <- table(pred, target)
   colnames(confMat) <- c("PRED FALSE", "PRED TRUE")
   row.names(confMat) <- c("TARGET FALSE", "TARGET TRUE")
   return(confMat)
@@ -178,3 +187,153 @@ legend('topright', c("log_reg","lda", "qda", "knn"),
 
 # TODO: Which model performs better?
 # TODO: Which model is interpretable and performs well?
+
+# Problem 2
+
+# read file
+id <- "1yYlEl5gYY3BEtJ4d7KWaFGIOEweJIn__" # google file ID
+d.corona <- read.csv(sprintf("https://docs.google.com/uc?id=%s&export=download", id),header=T)
+summary(d.corona)
+
+# Number of deceased and non-deceased
+deceased <- nrow(filter(d.corona, d.corona$deceased == "1"))
+non_deceased <- nrow(filter(d.corona, d.corona$deceased == "0"))
+
+# Number of males and females for each country
+number_of_males_and_females <- d.corona %>%
+  group_by(country, sex) %>%
+  summarise(amount = n()) %>%
+  arrange(desc(amount))
+
+
+# The number of deceased and non-deceased for each sex
+number_of_deceased_and_non_deceased <- d.corona %>%
+  group_by(deceased, sex) %>%
+  summarise(amount = n()) %>%
+  arrange(desc(amount)) 
+
+# The number of deceased and non-deceased in France, separate for each sex.
+number_of_deceased_and_non_deceased_in_France_for_each_sex <- filter(d.corona, d.corona$country == "France") %>%
+  group_by(deceased, sex) %>%
+  summarise(amount = n()) %>%
+  arrange(desc(amount)) 
+
+
+#Linear regression
+
+train_ID = sample(1:nrow(d.corona), nrow(d.corona)/(3/2))
+train_data = d.corona[train_ID, ]
+test_data = d.corona[-train_ID, ]
+
+age_vs_deceased <- d.corona %>%
+  group_by(age, deceased) %>%
+  summarise(amount = n()) %>%
+  arrange(desc(amount))
+
+age_country_deceased <- d.corona %>%
+  group_by(age, deceased, country) %>%
+  summarise(amount = n()) %>%
+  arrange(desc(amount)) 
+
+scatter_plot <- scatter.smooth(x=age_vs_deceased$age, y=age_vs_deceased$amount, main="Age ~ Deceased")
+cor(d.corona$age, d.corona$deceased)
+
+model <- glm(deceased ~ age + country + sex, family = binomial, data=train_data)
+anova(model)
+summary(model)
+coefficients(model)
+
+test <- filter( d.corona, d.corona$country == "Korea" & d.corona$age == "75" & d.corona$sex == "male")
+
+create_confusion_matrix <- function(pred, target) {
+  confMat <- table(pred, target)
+  colnames(confMat) <- c("PRED FALSE", "PRED TRUE")
+  row.names(confMat) <- c("TARGET FALSE", "TARGET TRUE")
+  return(confMat)
+}
+
+
+# 1. What is the probability to die of covid for a male age 75 living in Korea?
+data_point <- data.frame("age" = 75, "sex" = "male","country" = "Korea")
+predict(model, newdata = data_point, type = "response")
+# There are a 10.01 % chance of a 75 year-old korean male dying of covid. 
+
+# 2. Is there evidence that males have higher probability to die than females?
+# Yes, the regression model shows that there are a higher probability for males to die than females.
+# There is statistically significant evidence that males have a higher probability of dying of covid. 
+  
+# 3. Is there evidence that the country of residence has an influence on the probability to decease?
+# Yes, there is evidence that the country of residence has an impact on the probability to decease. There is a big difference between in the probability of decease in Korea, Japan, and Indonesia. Indonesia has a much higher probability of decease than the rest. 
+# There is evidence that for France and Indonesia, the country of residence does not impace the probability as much. While for Korea and Japan it has a bigger influence. This is due to the estimate of the dummy variables for France and Indonesia has a larger Pr(>|z|). 
+
+# 4. Quantify how the odds to die changes when someone with otherwise identical covariates is 10 years older than another person.
+summary(model)
+odds <- exp(10*coefficients(model)[2])
+
+# C.
+
+# Is age a greater risk factor for males than for females?
+
+# Is age a greater risk factor for the French population than for the Indonesian population?
+
+# D.
+
+lda_model <- lda(deceased ~ age + country + sex, data=d.corona)
+lda_pred <- predict(lda_model, newdata=test_data[-1])
+conf_matrix <- create_confusion_matrix(unlist(lda_pred[1]), test_data$deceased)
+sum(conf_matrix)
+null_rate <- (conf_matrix[2,1] + conf_matrix[2,2]) / sum(conf_matrix) 
+# 1. The "null rate" for misclassification is 5.22%, because this is the proportion of deaths among all cases in the dataset. 
+# False
+
+# 2. LDA is not a very useful method for this dataset.
+# False
+
+# 3. LDA has a specificity of 1.
+specificity <- conf_matrix[1,1] / (conf_matrix[1,1] + conf_matrix[1,2])
+# False
+
+# 4. QDA has a lower sensitivity to classify deceased compared to LDA.
+qda_model <- qda(deceased ~ ., data=d.corona)
+qda_pred <- predict(qda_model, newdata=test_data[-1])
+qda_conf <- create_confusion_matrix(unlist(qda_pred[1]), test_data$deceased)
+sens_lda <- conf_matrix[2,1] / (sum(conf_matrix[2,]))
+sens_qda <- qda_conf[2,1] / (sum(qda_conf[2,]))
+# False, they are equal
+
+
+
+# Problem 4
+
+
+
+# Problem 5
+id <- "19auu8YlUJJJUsZY8JZfsCTWzDm6doE7C" # google file ID
+d.bodyfat <- read.csv(sprintf("https://docs.google.com/uc?id=%s&export=download", id),header=T)
+
+bmi_model <- lm(bodyfat ~ age + weight + bmi, d.bodyfat)
+summary(bmi_model)$r.squared
+
+rsquared <- function(data, indices) {
+  d <- data[indices,] # select samples
+  fit <- lm(bodyfat ~ age + weight + bmi, d)
+  return(summary(fit)$r.squared)
+}
+
+rsquared(d.bodyfat)
+# The R^2 for a linear regression model is 0.5803
+
+set.seed(4268)
+
+# bootstrapping with 1000 replications
+results <- boot(data=d.bodyfat, statistic=rsquared,
+                R=1000)
+
+# view results
+results
+plot(results)
+
+# get 95% confidence interval
+boot.ci(results, type="bca")
+
+# There 
